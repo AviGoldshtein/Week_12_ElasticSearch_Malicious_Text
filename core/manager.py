4f1e913776101cc8dabea7c3e64756e2dc534cb8
avi <a0548589558@gmail.com>
2025-09-01 21:40:42 +0300

    there is a fast api server on

--- core/manager.py
+++ core/manager.py
@@ -12,13 +12,18 @@ class Manager:
 
     def initialize_data(self):
         self.logger.debug("initialize_data")
-        df = self.data_loader.load_data()
-        analyzer = Analyzer(df, index_name="malicious")
-        analyzer.create_index_and_mapping(index_name="malicious")
-        analyzer.insert_data_to_es(index_name="malicious")
-        analyzer.find_sentiments()
-        analyzer.find_detected_weapons()
-        analyzer.delete_non_relevant_rows()
+        # df = self.data_loader.load_data()
+        # analyzer = Analyzer(df, index_name="malicious")
+        # analyzer.create_index_and_mapping(index_name="malicious")
+        # analyzer.insert_data_to_es(index_name="malicious")
+        # analyzer.find_sentiments()
+        # analyzer.find_detected_weapons()
+        # analyzer.delete_non_relevant_rows()
 
     def get_all_data(self):
-        return self.es_connector.get_all_documents(index_name="malicious")
+        documents = self.es_connector.get_all_documents(index_name="malicious")
+        return [doc['_source'] for doc in documents]
+
+    def get_all_data_more_weapons(self):
+        data = self.get_all_data()
+        return [doc for doc in data if len(doc['weapons_detected']) >= 2]
--- main.py
+++ main.py
@@ -1,8 +1,52 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import uvicorn
 from core.manager import Manager
+from core.logger import logger
 import json
 
-
 manager = Manager()
-manager.initialize_data()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("the proses of initialize data started")
+    # print("the proses of initialize data started")
+    manager.initialize_data()
+    logger.info("the proses of initialize data finished")
+    # print("the proses of initialize data finished")
+    yield
+
+    print("the Enricher server finished")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+def health():
+    return {"health": "ok"}
+
+@app.get("/antisemitic_documents")
+def get_antisemitic_documents():
+    respo = manager.get_all_data()
+    return respo or {"msg": "there is nothing to show"}
+
+@app.get("/antisemitic_documents/weapons")
+def get_antisemitic_documents_with_weapons():
+    respo = manager.get_all_data_more_weapons()
+    return respo or {"msg": "there is nothing to show"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
+
+
+
+
+
+
+
+
+
+
+# manager = Manager()
+# manager.initialize_data()
 # res = manager.get_all_data()[0]
 # print(json.dumps(res, indent=4))
